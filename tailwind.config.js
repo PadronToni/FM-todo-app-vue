eb86f98ce19d513d76e3627b75ce49e4f606ee7c
Simone Boccuzzi <mughetsu2@gmail.com>
2022-08-28 10:50:51 +0200

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,21 @@ module.exports = {
       }
     },
     extend: {
+      fontFamily: {
+        sans: [
+          'Josefin Sans',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif'
+        ]
+      }
     }
   },
   plugins: [],
