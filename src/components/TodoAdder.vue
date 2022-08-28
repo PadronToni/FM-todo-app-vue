8631f8308e2caa949bd7372f8f9b5cd0ce54327c
Simone Boccuzzi <mughetsu2@gmail.com>
2022-08-28 19:21:28 +0200

    style basis

---
+++ src/components/TodoAdder.vue
@@ -0,0 +1,22 @@
+<template>
+  <form action="submit" method="post">
+    <label class="todo flex items-center gap-3">
+			<!-- check icon -->
+			<div class="w-5 h-5 rounded-full border border-neutral-light-200 dark:border-neutral-dark-400"></div>
+			<!-- text input -->
+      <input type="text" :placeholder="placeHolder" />
+    </label>
+  </form>
+</template>
+
+<script>
+const DEF_PLACEHOLDER = "Create a new to do...";
+</script>
+<script setup>
+const props = defineProps({
+  placeHolder: {
+    type: String,
+    default: DEF_PLACEHOLDER,
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
+import TodoAdder from "../components/TodoAdder.vue";
 </script>
 
 <template>
-  <main>
+  <main class="grid gap3 mx-10">
+    <TodoAdder class="shadow-2xl"></TodoAdder>
   </main>
 </template>
 
-<style>
-</style>
+<style></style>
