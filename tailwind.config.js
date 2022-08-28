fc87d8058f580cf0e4974664e0081fe30759b4d5
Simone Boccuzzi <mughetsu2@gmail.com>
2022-08-28 10:32:08 +0200

    color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,33 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        blue: 'hsl(220, 98%, 61%)',
+        cyan: 'hsl(192, 100%, 67%)',
+        fuchsia: 'hsl(280, 87%, 65%)'
+      },
+      'neutral-light': {
+        50: 'hsl(0, 0%, 98%)',
+        100: 'hsl(236, 33%, 92%)',
+        200: 'hsl(233, 11%, 84%)',
+        300: 'hsl(236, 9%, 61%)',
+        600: 'hsl(235, 19%, 35%)'
+      },
+      'neutral-dark': {
+        50: 'hsl(236, 33%, 92%)',
+        100: 'hsl(234, 39%, 85%)',
+        300: 'hsl(234, 11%, 52%)',
+        400: 'hsl(233, 14%, 35%)',
+        500: 'hsl(237, 14%, 26%)',
+        700: 'hsl(235, 24%, 19%)',
+        900: 'hsl(235, 21%, 11%)'
+      }
+    },
+    extend: {
+    }
   },
   plugins: [],
 }
