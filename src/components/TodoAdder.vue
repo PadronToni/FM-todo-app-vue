44d94f83ec1b806a55d1ba8325436256e46b0167
Simone Boccuzzi <mughetsu2@gmail.com>
2022-08-29 02:27:41 +0200

    base logic

--- src/components/TodoAdder.vue
+++ src/components/TodoAdder.vue
@@ -1,10 +1,12 @@
 <template>
-  <form action="submit" method="post">
+  <form action="" method="post" ref="formRef" @submit="onSubmit">
     <label class="todo flex items-center gap-3">
-			<!-- check icon -->
-			<div class="w-5 h-5 rounded-full border border-neutral-light-200 dark:border-neutral-dark-400"></div>
-			<!-- text input -->
-      <input type="text" :placeholder="placeHolder" />
+      <!-- check icon -->
+      <div
+        class="w-5 h-5 rounded-full border border-neutral-light-200 dark:border-neutral-dark-400"
+      ></div>
+      <!-- text input -->
+      <input type="text" name="todoAdder" :placeholder="placeHolder" />
     </label>
   </form>
 </template>
@@ -13,10 +15,30 @@
 const DEF_PLACEHOLDER = "Create a new to do...";
 </script>
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps({
   placeHolder: {
     type: String,
     default: DEF_PLACEHOLDER,
   },
 });
+
+const emit = defineEmits(["newTodo"]);
+
+const formRef = ref(null);
+
+function onSubmit(evt) {
+  // prevent normal submit flow ;)
+  evt.preventDefault();
+
+  // gets data form form and emit an event
+  const data = new FormData(formRef.value);
+  for (const [name, value] of data) {
+    value && emit("newTodo", value);
+  }
+
+  // resets the form
+  formRef.value.reset();
+}
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,20 @@
 <script setup>
+import { reactive } from "vue";
 import TodoAdder from "../components/TodoAdder.vue";
+const todos = reactive([]);
+
+function todoHandler(todo) {
+  todos.push(todo);
+}
 </script>
 
 <template>
-  <main class="grid gap3 mx-10">
-    <TodoAdder class="shadow-2xl"></TodoAdder>
+  <main class="grid gap3 p-4 mx-auto container">
+    <TodoAdder class="shadow-2xl" @new-todo="todoHandler"></TodoAdder>
+
+    <div class="grid divide-y my-3">
+      <div v-for="todo in todos" class="todo shadow-md">{{todo}}</div>
+    </div>
   </main>
 </template>
 
